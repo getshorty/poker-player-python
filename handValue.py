c7b41b27d137bce7592f45ad5a54d9453aee82e7
unknown <szszrenko@srvqa-n.NavNGo.local>
2014-05-28 10:52:42 +0200

    a

--- handValue.py
+++ handValue.py
@@ -9,7 +9,12 @@ class HandValue(object):
     def getValue(self):
         if self.card1['rank'] == 'A' and self.card2['rank'] == 'A':
             return 100
-
+        if self.card1['rank'] == 'K' and self.card2['rank'] == 'K':
+            return 90
+        if self.card1['rank'] == 'Q' and self.card2['rank'] == 'Q':
+            return 80
+        if self.card1['rank'] == 'J' and self.card2['rank'] == 'J':
+            return 70
         return 0
 
 hv =HandValue([{u'rank': u'J', u'suit': u'spades'}, {u'rank': u'A', u'suit': u'diamonds'}])
