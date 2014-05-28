0134c5df080d9a98b912f577d436363a55f343b9
QZ <qzoltan@gmail.com>
2014-05-28 13:48:20 +0200

    fullhouse fixed

--- cardValue.py
+++ cardValue.py
@@ -1,41 +1,35 @@
-__author__ = 'nberenyi'
+__author__ = 'qzoltan'
 
 class CardValue(object):
     PAIR=2
+    
     def __init__(self, cards):
         self.cards = cards
-        self.card1 = cards[0]
-        self.card2 = cards[1]
         
         self.equ = {}
 
     def getValue(self):
-##        if self.card1['rank'] == 'A' and self.card2['rank'] == 'A':
-##            return 100
-##        return 0
-
-        val = 0
+        rVal = 0
 
         self.equFunc()
         sames =  max( [ e  for e in self.equ.values() ] )
 
         # --- 1p, dr, po        
         if sames >= 1:
-          val = { 1:0, 2:10, 3:30, 4:70 }[ sames ]
+          rVal = { 1:0, 2:10, 3:30, 4:70 }[ sames ]
         
         # --- 2p
         doubles = self.countThem( 2 )
-        
         if doubles == 2:
-          val = max( val, 20 )
+          rVal = max( rVal, 20 )
           
         # --- fullhouse
         threes =  self.countThem( 3 )
-        if doubles == 1 and threes == 3:
-          val = max( val, 30 )
+        if doubles >= 1 and threes == 1:
+          rVal = max( rVal, 60 )
 
           
-        return val
+        return rVal
         
         
     # --- FUNC    
@@ -46,10 +40,6 @@ class CardValue(object):
         except:
            self.equ[ c['rank'] ]  = 1
            
-      ##print 1111, self.equ
-      ##return max( [ e  for e in self.equ.values() ] )
-      
-      
     def countThem( self, pThem ):
       vRet = 0
       for e in self.equ.values():
@@ -60,16 +50,26 @@ class CardValue(object):
       
         
       
-      
+# --- MAIN -----------------------------      
 if __name__ == '__main__':
+  # --- pok
   dicc = [{u'rank': u'J', u'suit': u'spades'}, {u'rank': u'J', u'suit': u'diamonds'}, {u'rank': u'J', u'suit': u'diamonds'}, {u'rank': u'J', u'suit': u'diamonds'}, {u'rank': u'K', u'suit': u'diamonds'}]
   hv =CardValue( dicc )
   print dicc
   print hv.getValue()
   print
 
+  # --- 2p
+  dicc = [{u'rank': u'J', u'suit': u'spades'}, {u'rank': u'J', u'suit': u'diamonds'}, {u'rank': u'2', u'suit': u'diamonds'}, {u'rank': u'2', u'suit': u'diamonds'}, {u'rank': u'K', u'suit': u'diamonds'}]
+  hv =CardValue( dicc )
+  print dicc
+  print hv.getValue()
+  print
+
+  # --- fullh
   dicc = [{u'rank': u'J', u'suit': u'spades'}, {u'rank': u'J', u'suit': u'diamonds'}, {u'rank': u'J', u'suit': u'diamonds'}, {u'rank': u'K', u'suit': u'diamonds'}, {u'rank': u'K', u'suit': u'diamonds'}]
   hv =CardValue( dicc )
   print dicc
   print hv.getValue()
   print
+  
