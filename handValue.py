66b8b6d740f6e18e4967ff80eae5f64123f890a9
unknown <szszrenko@srvqa-n.NavNGo.local>
2014-05-28 15:16:03 +0200

    sz

--- handValue.py
+++ handValue.py
@@ -6,6 +6,8 @@ class HandValue(object):
         self.cards = cards
         self.card1 = cards[0]
         self.card2 = cards[1]
+        self.card3 = cards[2]
+        self.card4 = cards[3]
 
     def getValue(self):
         if self.card1['rank'] == 'A' and self.card2['rank'] == 'A':
@@ -44,15 +46,21 @@ class HandValue(object):
             return 90
         if self.card1['rank'] == self.card2['rank']:
             return 100
+        if self.card1['rank'] in ["A", "K"] and self.card2['rank'] in ["T","9","8","7","6","5"]:
+            return 81
+        if self.card2['rank'] in ["A", "K"] and self.card1['rank'] in ["T","9","8","7","6","5"]:
+            return 81
+        if self.card1['rank'] in ["Q", "J"] and self.card2['rank'] in ["T","9","8","7","6","5"]:
+            return 79
+        if self.card2['rank'] in ["Q", "J"] and self.card1['rank'] in ["T","9","8","7","6","5"]:
+            return 79
         return 0
 
 
 
 if __name__ == "__main__":
     for x in range(0, 200):
-        rand1 = random.choice("AKQJT98765432")
-        rand2 = random.choice("AKQJT98765432")
-        #print rand1, rand2
-        hv3 =HandValue([{u'rank': u''+rand1+'', u'suit': u'spades'}, {u'rank': u''+rand2+'', u'suit': u'diamonds'}])
-        if hv3.getValue() > 90:
-            print rand1, rand2, hv3.getValue()
+        hv3 =HandValue([{u'rank': u''+random.choice("AKQJT98765432")+'', u'suit': u'spades'}, {u'rank': u''+random.choice("AKQJT98765432")+'', u'suit': u'diamonds'}, {u'rank': u''+random.choice("AKQJT98765432")+'', u'suit': u'diamonds'}, {u'rank': u''+random.choice("AKQJT98765432")+'', u'suit': u'diamonds'}])
+        if hv3.getValue() > 78:
+            #print "#",x,hv3.card1['rank'],hv3.card2['rank'],hv3.card3['rank'],hv3.card4['rank'], "Return:", hv3.getValue()
+            print "#",x,hv3.card1['rank'],hv3.card2['rank'], "Return:", hv3.getValue()
