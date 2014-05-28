9e17285b60f8ec6ef388ab646c00690207e20812
unknown <szszrenko@srvqa-n.NavNGo.local>
2014-05-28 11:37:24 +0200

    szszrenko@srvqa-n

--- handValue.py
+++ handValue.py
@@ -1,4 +1,5 @@
 __author__ = 'nberenyi'
+import random
 
 class HandValue(object):
     def __init__(self, cards):
@@ -6,18 +7,49 @@ class HandValue(object):
         self.card1 = cards[0]
         self.card2 = cards[1]
 
+    def getCards(self):
+        return self.card1['rank'] + self.card2['rank']
+
     def getValue(self):
         if self.card1['rank'] == 'A' and self.card2['rank'] == 'A':
             return 100
+        if self.card1['rank'] == 'A' and self.card2['rank'] == 'K':
+            return 90
+        if self.card1['rank'] == 'A' and self.card2['rank'] == 'Q':
+            return 90
+        if self.card1['rank'] == 'A' and self.card2['rank'] == 'J':
+            return 90
         if self.card1['rank'] == 'K' and self.card2['rank'] == 'K':
+            return 100
+        if self.card1['rank'] == 'K' and self.card2['rank'] == 'A':
+            return 90
+        if self.card1['rank'] == 'K' and self.card2['rank'] == 'Q':
+            return 90
+        if self.card1['rank'] == 'K' and self.card2['rank'] == 'J':
             return 90
         if self.card1['rank'] == 'Q' and self.card2['rank'] == 'Q':
-            return 80
+            return 100
+        if self.card1['rank'] == 'Q' and self.card2['rank'] == 'A':
+            return 90
+        if self.card1['rank'] == 'Q' and self.card2['rank'] == 'K':
+            return 90
+        if self.card1['rank'] == 'Q' and self.card2['rank'] == 'J':
+            return 90
         if self.card1['rank'] == 'J' and self.card2['rank'] == 'J':
-            return 70
+            return 100
+        if self.card1['rank'] == 'J' and self.card2['rank'] == 'A':
+            return 90
+        if self.card1['rank'] == 'J' and self.card2['rank'] == 'K':
+            return 90
+        if self.card1['rank'] == 'J' and self.card2['rank'] == 'Q':
+            return 90
+        #if self.card1['rank'] == self.card2['rank']:
+        #    return 100
         return 0
 
-hv =HandValue([{u'rank': u'J', u'suit': u'spades'}, {u'rank': u'A', u'suit': u'diamonds'}])
-print hv.getValue()
-
+#for x in range(0, 10):
+#    hv3 =HandValue([{u'rank': u''+random.choice("AKQJT98765432")+'', u'suit': u'spades'}, {u'rank': u''+random.choice("AKQJT98765432")+'', u'suit': u'diamonds'}])
+#    if hv3.getValue() > 90:
+#        print hv3.getCards()
+#        print hv3.getValue()
 
