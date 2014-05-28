3ac05cb1081f4a15e0c8e55ff74e3ef35e8598b6
QZ <qzoltan@gmail.com>
2014-05-28 13:35:08 +0200

    1p, 2p, d, pok

--- cardValue.py
+++ cardValue.py
@@ -14,25 +14,31 @@ class CardValue(object):
 ##            return 100
 ##        return 0
 
+        val = 0
+
         self.equFunc()
-        
         sames =  max( [ e  for e in self.equ.values() ] )
-        
+
+        # --- 1p, dr, po        
         if sames >= 1:
           val = { 1:0, 2:10, 3:30, 4:70 }[ sames ]
         
-        doubles = 0
-        for e in self.equ.values():
-          if e >= 2:
-            doubles +=1
-            
+        # --- 2p
+        doubles = self.countThem( 2 )
+        
         if doubles == 2:
           val = max( val, 20 )
+          
+        # --- fullhouse
+        threes =  self.countThem( 3 )
+        if doubles == 1 and threes == 3:
+          val = max( val, 30 )
 
           
         return val
         
         
+    # --- FUNC    
     def equFunc( self ):
       for c in self.cards:
         try:
@@ -43,20 +49,27 @@ class CardValue(object):
       ##print 1111, self.equ
       ##return max( [ e  for e in self.equ.values() ] )
       
+      
+    def countThem( self, pThem ):
+      vRet = 0
+      for e in self.equ.values():
+        if e >= pThem:
+          vRet += 1
+      return vRet
+        
+      
         
       
       
 if __name__ == '__main__':
   dicc = [{u'rank': u'J', u'suit': u'spades'}, {u'rank': u'J', u'suit': u'diamonds'}, {u'rank': u'J', u'suit': u'diamonds'}, {u'rank': u'J', u'suit': u'diamonds'}, {u'rank': u'K', u'suit': u'diamonds'}]
   hv =CardValue( dicc )
+  print dicc
   print hv.getValue()
+  print
 
   dicc = [{u'rank': u'J', u'suit': u'spades'}, {u'rank': u'J', u'suit': u'diamonds'}, {u'rank': u'J', u'suit': u'diamonds'}, {u'rank': u'K', u'suit': u'diamonds'}, {u'rank': u'K', u'suit': u'diamonds'}]
-    
   hv =CardValue( dicc )
-  print 
+  print dicc
   print hv.getValue()
-  
-  
-  
-  
+  print
